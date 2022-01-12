eeaabb82474c1fc02d7734397002d486fe04bc6a
MuratGuld <slimplus1952@gmail.com>
2022-01-13 00:29:15 +0100

    Century promramm was completed

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("deneme")
+const yil = 2000;
+
+const yuzyilBul = (yil) => {
+    if(yil%100==0) {
+        return `Girdiginiz tarih ${Math.floor(yil/100)}. yuzyila aittir.`
+    }
+    else {
+        return `Girdiginiz tarih ${Math.floor(yil/100)+1}. yuzyila aittir.`
+    }
+}
+
+console.log(yuzyilBul(yil))
